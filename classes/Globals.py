9f8df36c0600a0fd95a9cacd1e8f1218af363446
taciochi <ciochirca.teodoravram98@gmail.com>
2023-04-11 00:07:11 +0300

    Checkpoint: refactored globals

--- classes/Globals.py
+++ classes/Globals.py
@@ -5,15 +5,6 @@ from torch.cuda import is_available
 
 
 class Globals:
-    SEED_VAL: int = 3
-    GAMMA: float = 0.9
-    BATCH_SIZE: int = 75
-    GAME_HEIGHT: int = 512
-    MIN_EPSILON: float = 0.01
-    INIT_EPSILON: float = 0.5
-    MEMORY_SIZE: int = 1_000
-    EPSILON_DECAY: float = 0.97
-    NUMBER_OF_EPISODES: int = 1_200
     BRAIN_DIR_PATH: str = 'brains/'
     DEVICE_TYPE: device = device('cuda:0' if is_available() else 'cpu')
     REWARD_VALUES: Dict[str, float] = {
